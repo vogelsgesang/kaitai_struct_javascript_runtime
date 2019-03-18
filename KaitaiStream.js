69b97a328d77b4139092199ee9b3958d85b18e01
John Chadwick <jchw@google.com>
2019-03-17 23:47:35 -0700

    Fix operator precedence error with signed integer reading.

--- KaitaiStream.js
+++ KaitaiStream.js
@@ -213,7 +213,7 @@ KaitaiStream.prototype.readS8be = function(e) {
   var v1 = this.readU4be();
   var v2 = this.readU4be();
 
-  if (v1 & 0x80000000 != 0) {
+  if ((v1 & 0x80000000) != 0) {
     // negative number
     return -(0x100000000 * (v1 ^ 0xffffffff) + (v2 ^ 0xffffffff)) - 1;
   } else {
@@ -256,7 +256,7 @@ KaitaiStream.prototype.readS8le = function(e) {
   var v1 = this.readU4le();
   var v2 = this.readU4le();
 
-  if (v2 & 0x80000000 != 0) {
+  if ((v2 & 0x80000000) != 0) {
     // negative number
     return -(0x100000000 * (v2 ^ 0xffffffff) + (v1 ^ 0xffffffff)) - 1;
   } else {
